19074b3dc8c2de5e0798599338fed30dc02f3492
Makar Zverev <Panameraa888@gmail.com>
2024-02-08 16:42:04 +0300

    Написал минимальную рабочую версию

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿// Массив для обработки
+string[] inputStrings = new[]{"lorem", "ipsum", "dolor", "sit", "amet", "consectetuer",
+    "adipiscing", "elit", "sed", "diam", "nonummy", "nibh", "euismod",
+    "tincidunt", "ut", "laoreet", "dolore", "magna", "aliquam", "erat"};
+
+
+// Считаем количество коротких строк
+int outputArrayLength = 0;
+foreach (string inputString in inputStrings)
+{
+    if (inputString.Length <= 3)
+    {
+        outputArrayLength++;
+    }
+}
+// Создаем массив для коротких строк
+string[] shortStrings = new string[outputArrayLength];
+
+// Добавляем короткие строки в массив коротких строк
+int counter = 0;
+foreach (string inputString in inputStrings)
+{
+    if (inputString.Length <= 3)
+    {
+        shortStrings[counter++] = inputString;
+    }
+}
+
+// Выводим массив коротких строк в консоль
+foreach (string shortString in shortStrings)
+{
+    Console.WriteLine(shortString);
+}
+
