2ab48d3334b908557ae80807ead3e561cb224bde
Makar Zverev <Panameraa888@gmail.com>
2024-02-08 17:39:50 +0300

    Добавил функции для выбора режима ввода массива, для ручного ввода массива.

--- Program.cs
+++ Program.cs
@@ -4,9 +4,32 @@ string[] inputStrings = new[]{"lorem", "ipsum", "dolor", "sit", "amet", "consect
     "tincidunt", "ut", "laoreet", "dolore", "magna", "aliquam", "erat"};
 
 
+// Функция выбора режима ввода массива
+bool selectManualInput ()
+{
+    Console.WriteLine("Использовать ручной ввод массива строк?\nВведите Y для подтверждения.");
+    return (Console.ReadLine().ToUpper() == "Y");
+}
+
+
+// Функция ввода из консоли массива для обработки
+string[] stringsInput ()
+{
+    Console.WriteLine("Укажите количество слов в массиве");
+    int arrayLength = Int32.Parse(Console.ReadLine());
+    string[] strings = new string[arrayLength];
+    Console.WriteLine("Введите слова:");
+    for (int i = 0; i < strings.Length; i++)
+    {
+        strings[i] = Console.ReadLine();
+    }
+    return strings;
+}
+
+
 // Функция для красивого вывода массива в консоль
 // string[] strings - массив строк, который нужно вывести
-void OutputStringsToConsole(string[] strings)
+void OutputStringsToConsole (string[] strings)
 {
     string outputString = "[";
     foreach (string str in strings)
@@ -19,8 +42,17 @@ void OutputStringsToConsole(string[] strings)
 }
 
 
-void main()
+void main ()
 {
+    // Предлагаем пользователю ввести массив вручную вместо предустановленного.
+    if (selectManualInput())
+    {
+        inputStrings = stringsInput();
+    }
+
+    Console.WriteLine("Обрабатываемый массив:");
+    OutputStringsToConsole(inputStrings);
+
     // Считаем количество коротких строк
     int outputArrayLength = 0;
     foreach (string inputString in inputStrings)
@@ -42,7 +74,7 @@ void main()
             shortStrings[counter++] = inputString;
         }
     }
-
+    Console.WriteLine("Строки длинной <= 3 символам:");
     OutputStringsToConsole(shortStrings);
 }
 
